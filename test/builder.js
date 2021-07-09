70b07d357ffee335f79a00c98142b4b57caad73e
thomas.gafner <thomas.gafner@gmail.com>
2021-07-09 16:07:13 +0200

    More code cleanup.

--- test/builder.js
+++ test/builder.js
@@ -1,20 +1,19 @@
-const {schema} = require("prosemirror-schema-basic")
-// const {Schema} = require("prosemirror-model")
-const {builders} = require("prosemirror-test-builder")
+const {schema} = require('prosemirror-schema-basic')
+const {builders} = require('prosemirror-test-builder')
 
 const out = builders(schema, {
-  p: {nodeType: "paragraph"},
-	br: {nodeType: "hard_break"}
-  // pre: {nodeType: "code_block"},
-  // h1: {nodeType: "heading", level: 1},
-  // h2: {nodeType: "heading", level: 2},
-  // h3: {nodeType: "heading", level: 3},
-  // li: {nodeType: "list_item"},
-  // ul: {nodeType: "bullet_list"},
-  // ol: {nodeType: "ordered_list"},
-  // img: {nodeType: "image", src: "img.png"},
-  // hr: {nodeType: "horizontal_rule"},
-  // a: {markType: "link", href: "foo"},
+  p: {nodeType: 'paragraph'},
+	br: {nodeType: 'hard_break'}
+  // pre: {nodeType: 'code_block'},
+  // h1: {nodeType: 'heading', level: 1},
+  // h2: {nodeType: 'heading', level: 2},
+  // h3: {nodeType: 'heading', level: 3},
+  // li: {nodeType: 'list_item'},
+  // ul: {nodeType: 'bullet_list'},
+  // ol: {nodeType: 'ordered_list'},
+  // img: {nodeType: 'image', src: 'img.png'},
+  // hr: {nodeType: 'horizontal_rule'},
+  // a: {markType: 'link', href: 'foo'},
 })
 
 out.builders = builders
